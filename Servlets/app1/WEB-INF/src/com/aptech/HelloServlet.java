c9ddac8ccc7d7cabec38e860fa982313a410778d
Vijay_Ky <vijayky007@gmail.com>
2021-02-02 16:32:41 +0530

    updated

--- Servlets/app1/WEB-INF/src/com/aptech/HelloServlet.java
+++ Servlets/app1/WEB-INF/src/com/aptech/HelloServlet.java
@@ -4,7 +4,7 @@ import javax.servlet.http.*;//from servelts-api.jar file
 import java.io.*;//from jdk
 
 //every servlet should be a public class and
-  //should be subclass to HttoServlet
+  //should be subclass to HttpServlet
   //from HttpServlet several methods are inheriting to HelloServlet
 public class HelloServlet extends HttpServlet 
 {
@@ -17,3 +17,12 @@ public class HelloServlet extends HttpServlet
 		out.println("Hello from HelloServlet!");
 	}
 }
+/* 
+- in case of core java programs default executable method in a main which is static
+-in case of web apps default executable method is non-static service method
+-in every servlet service is the overrided method its available in the HttpServlet
+- public void service(HttpServletRequest request, HttpServletResponse response)
+  request is for for input purpose, response is output purpose
+- in order to print any output back to the browser, need to use printwriter not the system.out
+-
+*/
